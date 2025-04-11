5cb829607a61b8af4f179d0f61c993ee4444fd31
ShreyaRai20 <shreya200199@gmail.com>
2025-04-11 13:52:54 -0500

    added more

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 
 # Instance
 app = FastAPI()
 
+# published: bool - returns string so need to specify this
 
-@app.get('/') # pathOperationDecorator.operation(path/route)
-def index(): # path operation function
-    return {'data': {'name':'Shreya'}}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog') # pathOperationDecorator.operation(path/route)
+def index(limit=10, published: bool=True, sort: Optional[str]=None): # path operation function
+    # Only get 10 published bogs
+    if published:
+        return {'data': f'{limit} blogs from the db'}
+    else:
+        return {'data': f'published false'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments with blog with id = id
+    return {'data': ['1','2', limit]}
+
+
+class Blog(BaseModel):
+    id: int
+    title: str
+    body: str
+    published_at: Optional[bool] = True
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'published blog with {blog.title}'}
