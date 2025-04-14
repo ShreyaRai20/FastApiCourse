f14d343bfd57c9e02db0663360d0a22ea71e1875
ShreyaRai20 <shreya200199@gmail.com>
2025-04-14 16:44:25 -0500

    created database and more

---
+++ blog/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm.session import Session
+from .schemas import Blog
+from . import models
+from .database import engine, SessionLocal
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
+
+
+# Create
+@app.post('/blog')
+def create(blog: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, body=blog.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def get_all_blog(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def get_all_blog(id,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
+
+
